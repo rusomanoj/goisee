2ec0f0f95a220779a56907487b09c73d305104b3
Manoj <rusomanoj88@gmail.com>
2013-11-20 10:28:58 +0530

    home.php

--- application/views/home.php
+++ application/views/home.php
@@ -3,16 +3,16 @@
     <div class="image_resize"
 	 <ul class="grid effect-4" id="grid" style="position: relative;">
 <?php    
-//
-//foreach($see as $s)
-//{
-//   // echo '<pre>';
-//   // print_r($s);
-//  
-//    echo '<img src ='.str_replace('localhost/goisee/','',$s->path).' width="25%" height="25%">';
-////    echo "<br/>";
-////    echo "<br/>";
-//}
+
+foreach($see as $s)
+{
+   // echo '<pre>';
+   // print_r($s);
+  
+    echo '<img src ='.str_replace('localhost/goisee/','',$s->path).' width="25%" height="25%">';
+//    echo "<br/>";
+//    echo "<br/>";
+}
 ?>
 </ul>
 <!--</div>-->
